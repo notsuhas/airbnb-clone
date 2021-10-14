d8ac25df298a88ec931d82a84beae183c64de411
Suhas Siriprole <suhas.siriprole@gmail.com>
2021-10-14 10:32:44 +0530

    search page complete

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,52 @@ import {
 	UsersIcon,
 	MenuIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
 
-function Header() {
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
+
+function Header({placeholder}) {
+	const [searchInput, setSearchInput] = useState("");
+	const [numOfGuests, setNumOfGuests] = useState(1);
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+	const router = useRouter();
+
+	const handleSelect = (ranges) => {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	};
+
+	const resetInput = () => {
+		setSearchInput("");
+	};
+
+	const search = () => {
+		router.push({
+			pathname: "/search",
+			query: {
+				location: searchInput,
+				startDate: startDate.toISOString(),
+				endDate: endDate.toISOString(),
+				numOfGuests,
+			},
+		});
+	};
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: "selection",
+	};
+
 	return (
 		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-			<div className="relative flex items-center h-10 cursor-pointer my-auto">
+			<div
+				onClick={() => router.push("/")}
+				className="relative flex items-center h-10 cursor-pointer my-auto">
 				<Image
 					src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
 					layout="fill"
@@ -20,9 +61,11 @@ function Header() {
 			</div>
 			<div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
 				<input
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value)}
 					className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
 					type="text"
-					placeholder="Start your search..."
+					placeholder={placeholder || "Start your search..."}
 				/>
 				<SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
 			</div>
@@ -34,6 +77,38 @@ function Header() {
 					<UserCircleIcon className="h-6 cursor-pointer" />
 				</div>
 			</div>
+
+			{searchInput && (
+				<div className="flex flex-col col-span-3 mx-auto">
+					<DateRangePicker
+						ranges={[selectionRange]}
+						minDate={new Date()}
+						rangeColors={["#fd5861"]}
+						onChange={handleSelect}
+					/>
+					<div className="flex items-center border-b mb-4">
+						<h2 className="text-2xl flex-grow font-semibold">
+							Number of Guests
+						</h2>
+						<UsersIcon className="h-5" />
+						<input
+							type="number"
+							value={numOfGuests}
+							onChange={(e) => setNumOfGuests(e.target.value)}
+							min={1}
+							className="w-12 pl-2 text-lg outline-none text-red-400"
+						/>
+					</div>
+					<div className="flex">
+						<button className="flex-grow text-gray-500" onClick={resetInput}>
+							Cancel
+						</button>
+						<button className="flex-grow text-red-400" onClick={search}>
+							Search
+						</button>
+					</div>
+				</div>
+			)}
 		</header>
 	);
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
 	images: {
-		domains: ["upload.wikimedia.org", "a0.muscache.com"],
+		domains: [
+			"upload.wikimedia.org",
+			"a0.muscache.com",
+			"www.expatkings.com",
+			"www.smartertravel.com",
+			"cdn.bisnow.net",
+			"media.cntraveler.com",
+			"static.trip101.com",
+			"image.insider.com",
+		],
 	},
 };
